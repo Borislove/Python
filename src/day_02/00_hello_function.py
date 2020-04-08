395745af56166f9169d8a26140bb033a51a1a5b5
Boris <abirme@yandex.ru>
2020-04-08 13:53:12 +0300

    Интенсив SkillBox Python , day 2

---
+++ src/day_02/00_hello_function.py
@@ -0,0 +1,17 @@
+"""
+Пример программы для работы с функциями
+Сделать
+- функцию hello, которая выводит текст приветствия клиенту
+"""
+def user_hello(user: str):
+    print(f"Hello, {user}")
+
+clients = ['John', 'David', 'Kate', 'Alex']
+
+for user in clients:
+    user_hello()
+
+clients = ['Edward']
+
+for user in clients:
+    user_hello(user)
